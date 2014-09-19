19abb14074ab6081f91c79eddb606f51b3179fad
lwan985 <ashnh@hotmail.com>
2014-09-19 17:45:17 +1000

    V0.1 Minor modifications Solve the subset 1 problem using sample code with little adaptations.

--- python2perl.pl
+++ python2perl.pl
@@ -16,5 +16,22 @@ else {
 
 sub process_line() {
     my $line = $_[0];
-    print $line;
+	if ($line =~ /^#!/ && $. == 1) {
+		# translate #! line 
+		print "#!/usr/bin/perl -w\n";
+	}
+	elsif ($line =~ /^\s*#/ || $line =~ /^\s*$/) {
+		# Blank & comment lines can be passed unchanged
+		print $line;
+	}
+	elsif ($line =~ /^\s*print\s*"(.*)"\s*$/) {
+		# Python's print print a new-line character by default
+		# so we need to add it explicitly to the Perl print statement
+		print "print \"$1\", \"\\n\";\n";
+	}
+	else {
+		# Lines we can't translate are turned into comments
+		print "#$line\n";
+	}
 }
+
